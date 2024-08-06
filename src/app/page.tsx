bc3e029903477e821c0ffa05a238eb6058b8988f
Nishant Balaji <nishantb1130@gmail.com>
2024-08-06 01:03:34 -0700

    update metadata and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ import About from "@/components/About";
 const Home: NextPage = () => {
   return (
     <main className={styles.main}>
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🤖</text></svg>"
+      />
       <About />
     </main>
   );
